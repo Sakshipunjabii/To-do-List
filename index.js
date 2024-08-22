027909b99eee7453684446e029f87d0a189a10ae
Sakshipunjabii <punjabisakshi10@gmail.com>
2024-08-23 00:16:27 +0530

    update index.css and index.js files

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const container=document.querySelector('.container');
-container.addEventListener('click',function(event){
-event.preventDefault();
-})
+
 
 const add=document.querySelector('#add_task');
-add.addEventListener('click',function(){
 
-const task=document.querySelector('#task').value;
-document.querySelector('#new_task').innerHTML='<label class="content" for="task4"><input type="checkbox" name="task" id="task4">{task}<input class="icon" id="d_task4" type="image" src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png" alt="Delete"></label><br>'
 
-})
+add.addEventListener('click',function(event){
+    event.preventDefault();
+    
+const task =document.querySelector('#task').value;
+const newtask= '<input type="checkbox" name="task" id="task${task}"><label class="content" for="task${task} d_task${task}">' +task+ '</label><input class="icon" id="d_task${task}" type="image" src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png" alt="Delete"><br>';
+document.querySelector('#new_task').innerHTML += newtask;
+
+document.querySelector('#task').value='';
+
+const d=document.querySelector('#d_task${task}');
+
+d.addEventListener('click',function(event){
+    event.preventDefault();
+    
+document.querySelector('#task${task}').parentElement.remove();
+
+});
+});
+
+
